eb3958a4c890c46b6b4d81dca51ce2df08cf86d8
Anand P <84928066+anandp2002@users.noreply.github.com>
2024-07-03 00:05:01 +0530

    Time: 34 ms (66.96%), Space: 16.4 MB (79.72%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -3,23 +3,23 @@ class Solution:
         res = []
         direction=0
         left,right=0,len(matrix[0])-1
-        top,down=0,len(matrix)-1
+        top,bottom=0,len(matrix)-1
 
-        while(left<=right and top<=down):
+        while(left<=right and top<=bottom):
             if(direction==0):
                 for i in range(left,right+1):
                     res.append(matrix[top][i])
                 top+=1
             elif(direction==1):
-                for i in range(top,down+1):
+                for i in range(top,bottom+1):
                     res.append(matrix[i][right])
                 right-=1
             elif(direction==2):
                 for i in range(right,left-1,-1):
-                    res.append(matrix[down][i])
-                down-=1
+                    res.append(matrix[bottom][i])
+                bottom-=1
             else:
-                for i in range(down,top-1,-1):
+                for i in range(bottom,top-1,-1):
                     res.append(matrix[i][left])
                 left+=1
             direction=(direction+1)%4
